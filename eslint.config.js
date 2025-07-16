7d3d276b8c289cdeb925df7129c36b12f38cf534
EduardooPV <eduardoveltroni@hotmail.com>
2025-07-16 19:52:41 -0300

    feat: initial project

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginTs from '@typescript-eslint/eslint-plugin';
+import parserTs from '@typescript-eslint/parser';
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: parserTs,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': eslintPluginTs,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-console': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/strict-boolean-expressions': 'error',
+      '@typescript-eslint/no-floating-promises': 'error',
+    },
+  },
+];
