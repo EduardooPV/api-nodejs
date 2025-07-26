c700f8c96dd70ba30b65d17f613eddbafb0d2687
EduardooPV <eduardoveltroni@hotmail.com>
2025-07-26 19:58:56 -0300

    feat: create postgres db with docker-compose

--- src/infra/repositories/PostgresUsersRepository.ts
+++ src/infra/repositories/PostgresUsersRepository.ts
@@ -1,15 +1,29 @@
-import { User } from '../../domains/users/entities/User';
+import { prisma } from '../database/prismaClient';
 import { IUsersRepository } from '../../domains/users/repositories/IUserRepository';
+import { User } from '../../domains/users/entities/User';
 
 class PostgresUsersRepository implements IUsersRepository {
-  private users: User[] = [];
-
   async findByEmail(email: string): Promise<User | null> {
-    return this.users.find((user) => user.email === email) || null;
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (user === null) {
+      return null;
+    }
+
+    return user;
   }
 
   async create(user: User): Promise<void> {
-    this.users.push(user);
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        id: crypto.randomUUID(),
+      },
+    });
   }
 }
 
